49b540a0171d9397a4f4dab8917c5a87e8a270d5
Fatih Cigirci <cigircii@users.noreply.github.com>
2022-11-03 17:05:35 +0100

    Updated .gitignore

---
+++ Cigirci.Budgeteer.API/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿namespace Cigirci.Budgeteer.API.Controllers;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(AuthenticationSchemes = "Bearer")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var name = User?.Identity?.Name;
+        var claim = User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        
+        return Ok($"Hello {name} ({claim})");
+    }
+}
