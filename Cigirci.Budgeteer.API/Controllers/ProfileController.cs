2209ac0b545ed65c3693160a278656cd525e6b5e
Fatih Cigirci <fatih.cigirci@companial.com>
2022-11-13 05:54:26 +0100

    Added empty controllers for new models

---
+++ Cigirci.Budgeteer.API/Controllers/ProfileController.cs
@@ -0,0 +1,15 @@
+namespace Cigirci.Budgeteer.API.Controllers;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Attributes;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Properties;
+
+[Authorize]
+[ODataQueryParameterBinding]
+[ODataRouteComponent(ODataProperties.ODataRoutePrefix)]
+public class ProfileController : ODataController
+{
+    
+}
