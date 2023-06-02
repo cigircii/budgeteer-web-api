adf51da3dba26b0a39d5f678f6c17c4791f1a2c2
Fatih Cigirci <fatih.cigirci@companial.com>
2023-06-02 14:44:50 +0100

    Added auth to swagger gen
    Added the possibility to add a JWT Bearer token to the header of a call within Swagger for executing authenticated calls internally

--- Cigirci.Budgeteer.API/Program.cs
+++ Cigirci.Budgeteer.API/Program.cs
@@ -38,10 +38,10 @@ builder.Services.AddControllers()
     }).AddOData(options =>
     {
         options.Filter()
-        .Select()
-        .OrderBy()
-        //.Expand()
-        .SetMaxTop(5000);
+            .Select()
+            .OrderBy()
+            //.Expand()
+            .SetMaxTop(5000);
     });
 
 //TODO: Add hosted service
@@ -63,6 +63,31 @@ builder.Services.AddSwaggerGen(c =>
             Description = OpenApiInfoProperties.Description,
             Contact = OpenApiInfoProperties.Contact
         });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
 
     c.EnableAnnotations();
 
